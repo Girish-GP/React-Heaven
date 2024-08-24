39606cc8d47f7151031aa1dd47b1eed04d6c2908
Girish Parulekar <113516103+Girish-GP@users.noreply.github.com>
2024-08-24 06:19:38 +0000

    testing

--- src/utils/CartSlice.js
+++ src/utils/CartSlice.js
@@ -1,4 +1,36 @@
 import {createSlice} from "@reduxjs/toolkit"
+import { useSelector } from "react-redux";
+
+const dataList = useSelector((store)=> store?.cart?.items)
+console.log(dataList)
+const checkIfPresent = (dataList) =>{
+    dataList?.map((obj)=>{
+        let temp = false;
+        if(obj?.itemData?.card?.info?.id == action?.payload?.card?.info?.id){
+            temp = true
+            objIndex = index;
+        } 
+        return temp;
+    })
+}
+const addCartItemFunction = (state,action)=>{
+    let objIndex = null;
+ 
+    if(checkIfPresent(dataList)){
+        //update count and total price for existing item
+        if(objIndex){
+            state.items[objIndex].itemCount++;
+            state.items[objIndex].itemTotalPrice = state.items[objIndex].itemTotalPrice * state.items[objIndex].itemCount;
+        }
+    } else {
+        //Push new item
+        state?.items?.push({
+            itemData: action?.payload,
+            itemCount: 1,
+            itemTotalPrice: Number(action?.payload?.card?.info?.price)/100
+        });
+    }
+}
 
 const CartSlice = createSlice({
     name: 'cart',
@@ -7,11 +39,7 @@ const CartSlice = createSlice({
     },
     reducers:{
         addCartItem: (state,action)=>{
-            state?.items?.push({
-                itemData: action?.payload,
-                itemCount: 1,
-                itemTotalPrice: Number(action?.payload?.card?.info?.price)/100
-            });
+           addCartItemFunction(state,action)
         },
         removeCartItem: (state)=>{
             state?.items?.pop()
