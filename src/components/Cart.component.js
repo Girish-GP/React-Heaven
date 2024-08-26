bcc2efa534a0fbe09bfa5ef278a78b7ccd32a559
Girish Parulekar <113516103+Girish-GP@users.noreply.github.com>
2024-08-26 14:05:24 +0000

    cart total added

--- src/components/Cart.component.js
+++ src/components/Cart.component.js
@@ -4,19 +4,27 @@ import { Link } from "react-router-dom";
 import { EMPRT_CART_URL } from "../utils/constants";
 
 const CartComponent = () => {
-  
+     const sum = (data)=>{
+        const sum = data?.reduce((acc,cv)=> {
+            acc = acc + cv?.itemTotalPrice;
+            return acc
+          },0)
+          return sum?.toFixed(2)
+     }
     const cartData = useSelector((store)=> store?.cart?.items);
            return  (
             <div className="flex justify-center items-center flex-col mt-8">
                 <div  className="font-bold text-3xl text-red-400 mb-2">
                     YOUR CART
-                </div>
-               
+                </div>         
                  { cartData?.length ? 
                 cartData.map((obj)=>{
                     
                     return <CartSectionComponent key={obj?.itemData?.card?.info?.name} obj={obj}/>
-                }) : (
+                } 
+                
+                
+            ) : (
                     <div className="w-2/4 h-full shadow-sm p-8 flex flex-col items-center">
                         <img className="w-2/4" src={EMPRT_CART_URL} alt="cart empty"></img>
                     <p className="text-xl mt-4 font-medium">Your cart is empty</p>
@@ -24,6 +32,17 @@ const CartComponent = () => {
                     <Link className="text-xl font-bold bg-red-400 text-white rounded-sm p-2" to="/" >SEE RESTUARANTS NEAR YOU</Link>
                 </div>   
                 )
+               }
+               {
+                cartData?.length && sum(cartData) && <div className="border-none mt-4 rounded-xl shadow-lg w-80 p-8">
+                <p className="font-bold text-xl">Cart Total</p>
+                <div className="mt-2 flex justify-between">
+                    <p>Total</p>
+                    <span>{
+                        sum(cartData)
+                    }</span>
+                </div>
+               </div>  
                }
             </div>
            )
