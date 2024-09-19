da1cf2f52313b032a0da54ac5c2456728bb3e1c7
Girish Parulekar <113516103+Girish-GP@users.noreply.github.com>
2024-09-19 13:55:43 +0000

    header basic test cases

--- src/components/__test__/header.test.js
+++ src/components/__test__/header.test.js
@@ -1,5 +1,5 @@
 import{ HeaderComponent} from "../Header.component"
-import { render,screen } from "@testing-library/react"
+import { fireEvent, render,screen } from "@testing-library/react"
 import { Provider } from "react-redux"
 import AppStore from "../../utils/AppStore"
 import { BrowserRouter } from "react-router-dom"
@@ -14,7 +14,54 @@ it("should load header component with login button",()=>{
     
   );
 
-  const loginBtn = screen?.getByRole('button');
+  const loginBtn = screen?.getByRole('button',{name: 'Login'});
 
   expect(loginBtn).toBeInTheDocument()
+})
+
+it("Should load with cart items with zero items",()=>{
+  render(
+    <BrowserRouter>
+    <Provider store={AppStore}>
+      <HeaderComponent/>
+    </Provider>
+    </BrowserRouter>
+  )
+
+  const cartItems = screen?.getByText('Cart(0 items)')
+
+  expect(cartItems)?.toBeInTheDocument()
+})
+
+it("Should load with cart items",()=>{
+  render(
+    <BrowserRouter>
+    <Provider store={AppStore}>
+      <HeaderComponent/>
+    </Provider>
+    </BrowserRouter>
+  )
+
+  const cartItems = screen?.getByText(/Cart/)
+
+  expect(cartItems)?.toBeInTheDocument()
+})
+
+it("Should chane login button to logout on click",()=>{
+  render(
+    <BrowserRouter>
+    <Provider store={AppStore}>
+      <HeaderComponent/>
+    </Provider>
+    </BrowserRouter>
+  )
+
+  const loginBtn = screen?.getByRole('button',{name: 'Login'})
+ 
+  fireEvent.click(loginBtn);
+
+  const loginOutBtn = screen?.getByRole('button',{name: 'Logout'})
+
+  expect(loginOutBtn)?.toBeInTheDocument()
+
 })
